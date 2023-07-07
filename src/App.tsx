0e8feff1feee55050cbf0f675611d629d9139f5d
nnivxix <adhe.hanasa@gmail.com>
2023-07-07 18:32:11 +0700

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <main className="h-screen">Content</main>
+      <main className="h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </main>
       <Footer />
     </Router>
   );
