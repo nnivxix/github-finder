bef9320b951c034a63c267f941a013f9793540a9
nnivxix <adhe.hanasa@gmail.com>
2023-07-07 17:57:53 +0700

    feat: navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
-	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</>
-	);
+  return (
+    <Router>
+      <Navbar />
+    </Router>
+  );
 }
 
 export default App;
+
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { DiGithubBadge } from "react-icons/di";
+
+function Navbar() {
+  return (
+    <nav className="navbar shadow-lg mb-12 flex justify-between">
+      <div className="flex-none px-2 mx-2">
+        <DiGithubBadge className="text-2xl" />
+        <Link to="/" className="text-lg font-bold align-middle">
+          <h1>Github Finder</h1>
+        </Link>
+      </div>
+      <div className=" px-2 mx-2">
+        <div className="flex-justify-end">
+          <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+            Home
+          </Link>
+          <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+            About
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
