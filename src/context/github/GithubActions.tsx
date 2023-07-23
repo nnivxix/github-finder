ca42efd0401f1ff1752208daa5fe4145b95b2e17
nnivxix <adhe.hanasa@gmail.com>
2023-07-23 18:44:16 +0700

    fix: remove unused function

--- src/context/github/GithubActions.tsx
+++ src/context/github/GithubActions.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { UserGithub, UserRepo } from "../../types/schema";
+import { UserGithub } from "../../types/schema";
 
 const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL;
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
@@ -16,23 +16,6 @@ export const searchUsers = async (text: string) => {
   const { items }: { items: UserGithub[] } = response.data;
 
   return items;
-};
-
-export const getUserRepos = async (login: string) => {
-  const response = await github.get(
-    `${GITHUB_API_URL as string}/users/${login}/repos?sort=updated&per_page=10`
-  );
-  return response.data as UserRepo;
-};
-
-export const getUser = async (login: string) => {
-  const response = await fetch(`${GITHUB_API_URL as string}/users/${login}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN as string}`,
-    },
-  });
-  const data: UserGithub = await response.json();
-  return data;
 };
 
 export const getUserAndRepos = async (login: string) => {
